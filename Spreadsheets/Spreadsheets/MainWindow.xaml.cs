a3d9470fa7b5b08a6a24794a28e7aad951b9ec0e
Marko Injac <marko.injac05@gmail.com>
2022-08-14 18:02:31 +0200

    Konacni req i pocetak

---
+++ Spreadsheets/Spreadsheets/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace Spreadsheets
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataTable dt = new DataTable();
+            for (int i = 0; i < 1000; i++)
+            {
+                dt.Columns.Add();
+                dt.Rows.Add();
+            }
+            dg.ItemsSource = dt.DefaultView;
+        }
+        private void dg_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            
+        }
+    }
+}
